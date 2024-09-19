d5ff84884b708cd8b14fea588c2bb19a312f1325
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-19 04:30:42 -0400

    Create assignShift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -20,3 +20,28 @@ displayEmployeeShifts(employee);});
 /* Function ensures that the function goes through every employee and displays the information regarding all employees. 
 Output matches information in the data set.
 Task 2 is now complete. */
+
+// Task 3: Create a Function to Assign a New Shift
+function assignShift(employeeName, day, hours) {
+let employee;
+for (let staffMember of employees) {
+if (staffMember.name === employeeName) {
+employee = staffMember;}}
+// Loop has been created to ensure function goes through every employee to find desired employee information.
+if (!employee) {console.log("Error: " + employeeName + " is not an employee.");
+// Function meant to display message if employee listed is not found. 
+return;}
+for (let shift of employee.shifts) {
+if (shift.day === day) {
+console.log("Error: " + employeeName + "'s shift is already scheduled on " + day + ".");
+return;}}
+// Function meant to display error message if employee shift is already scheduled.
+employee.shifts.push({ day: day, hours: hours });
+console.log("Operation successful! A new shift has been assigned for " + employeeName + " for " + hours + " hours" + " on " + day + '.');}
+// Function meant to display message if a new shift has been assigned. 
+// Testing function from all aspects.
+assignShift("Ayla", "Monday", 7);
+assignShift("Andy", "Monday", 5);
+assignShift("William", "Wednesday", 8);
+// Function displayed accurate output and messages. 
+// Task 3 is now complete. 
