e361f78ef41b518c86a0e4884b265bc77b4600ea
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-19 03:31:02 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,9 @@
+// Task 1: Create an Employees Array of Employee Objects
+const employees = [
+{ name: 'Tatiana', shifts: [{ day: 'Tuesday', hours: 7 }, { day: 'Friday', hours: 8 }] },
+{ name: 'Ayla', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesdy', hours: 5 }] },
+{ name: 'Andy', shifts: [{ day: 'Wednesday', hours: 6 }] },
+{ name: 'Liam', shifts: [{ day: 'Tuesday', hours: 8 }] }];
+console.log(employees);
+// Array has been created and contains the data set that will be used for this challenge. 
+// Task 1 is now complete. 
