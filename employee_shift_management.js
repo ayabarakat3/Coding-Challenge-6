c9dd0af3bc12af3061f4d4242cc8392f4cc55682
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-19 03:44:46 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -7,3 +7,16 @@ const employees = [
 console.log(employees);
 // Array has been created and contains the data set that will be used for this challenge. 
 // Task 1 is now complete. 
+
+// Task 2: Create a Function to Display Employee Shift Details
+function displayEmployeeShifts(employee) {
+console.log("Name: " + employee.name);
+console.log("Employee shifts:");
+employee.shifts.forEach(function(shift) {
+console.log("Working day: " + shift.day + ", Working duration in hours: " + shift.hours);});}
+// This function is meant to display the information regarding the employees and their shifts during the week. 
+employees.forEach(function(employee) {
+displayEmployeeShifts(employee);}); 
+/* Function ensures that the function goes through every employee and displays the information regarding all employees. 
+Output matches information in the data set.
+Task 2 is now complete. */
